6ae8373ecd3a79efe7fa77f4633f379e86cac75e
mylesjsantiago48 <70586755+mylesjsantiago48@users.noreply.github.com>
2020-10-22 19:57:01 -0400

    Did part of HTML for movie_picker

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app"><br>
+    <router-link to="/"><h1>Movie Picker</h1></router-link>
+    <hr>
+    <div id="nav">
+      <h4> Pick the genre movie you're in the mood to watch. </h4>
+    </div>
+    <router-view/>
+    <br>
+
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+#h1{
+  color: blue;
+}
+.b-button{
+  color: violet;
+  text-decoration-color: violet;
+  margin-top: 3 !important;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+  
+  <b-container>
+    <b-row class="b-row" align-h="center" col="auto">
+      <b-col><b-button variant="info">Action</b-button></b-col>
+      <b-col><b-button variant="info">Adventure</b-button></b-col>
+      <b-col><b-button variant="info">Animation</b-button></b-col>
+      <b-col><b-button variant="info">Comedy</b-button></b-col>
+    </b-row><br>
+    <b-row class="b-row" align-h="center">
+      <b-col><b-button variant="info">Crime</b-button></b-col>
+      <b-col><b-button variant="info">Drama</b-button></b-col>
+      <b-col><b-button variant="info">Fantasy</b-button></b-col>
+      <b-col><b-button variant="info">Historical</b-button></b-col>
+    </b-row><br>
+    <b-row align-h="center">
+      <b-col><b-button variant="info">Horror</b-button></b-col>
+      <b-col><b-button variant="info">Magical Realism</b-button></b-col>
+      <b-col><b-button variant="info">Mystery</b-button></b-col>
+      <b-col><b-button variant="info">Paranoid Fiction</b-button></b-col>
+    </b-row><br>
+    <b-row align-h="center">
+      <b-col><b-button variant="info">Philosophical</b-button></b-col>
+      <b-col><b-button variant="info">Politcal</b-button></b-col>
+      <b-col><b-button variant="info">Romance</b-button></b-col>
+      <b-col><b-button variant="info">Saga</b-button></b-col>
+    </b-row><br>
+    <b-row align-h="center">
+      <b-col><b-button variant="info">Satire</b-button></b-col>
+      <b-col><b-button variant="info">Science Fiction</b-button></b-col>
+      <b-col><b-button variant="info">Social</b-button></b-col>
+      <b-col><b-button variant="info">Speculative</b-button></b-col>
+    </b-row><br>
+    <b-row align-h="center">
+      <b-col><b-button variant="info">Thriller</b-button></b-col>
+      <b-col><b-button variant="info">Urban</b-button></b-col>
+      <b-col><b-button variant="info">Western</b-button></b-col>
+      <b-col><b-button variant="info">Other</b-button></b-col>
+    </b-row>
+  </b-container>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.b-row{
+  color: violet;
+  text-decoration-color: violet;
+  margin-bottom: 3 !important;
+}
+</style>
+
